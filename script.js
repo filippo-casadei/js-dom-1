3301cc23b7ff5f0bb62df6a13115a691b88ab0ba
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-02 17:00:19 +0200

    Aggiunti i miei viaggi mentali in italiano sul file script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// STEP DEI MIEI RAGIONAMENTI MENTALI TRADOTTI IN ITALIANO:
+// mi servono sicuramente 2 elementi dal DOM:
+// - lampadina, alla quale ho dato apposta l'id perchè va sostituita con un'altra immagine
+// - interruttore, perchè è quello che deve scatenare l'evento click

98610afe2df2b48f722dc6ad2bfb1ec6fa4ae4fe
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-02 17:13:30 +0200

    modifiche al file script.js: - create 2 variabiili che vanno a prendere dal DOM i selettori lampadina e interruttore.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 // mi servono sicuramente 2 elementi dal DOM:
 // - lampadina, alla quale ho dato apposta l'id perchè va sostituita con un'altra immagine
 // - interruttore, perchè è quello che deve scatenare l'evento click
+
+// Creo due variabili, alle quali do per comodità lo stesso nome che ha il selettore,
+// che vanno a prendere dal DOM i selettori lampadina e interruttore, per poterle
+// ri utilizzare.
+
+//document.querySelector(`#lampadina`);
+const lampadina = document.querySelector(`#lampadina`);
+
+// document.querySelector(`#interruttore`);
+const interruttore = document.querySelector(`#interruttore`);
